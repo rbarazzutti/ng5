ee73dc5e11b9e0a40bda5e19e47714d8bfb71248
Raphael P. Barazzutti <raphael@barazzutti.net>
2018-04-25 18:55:51 +0200

    ajout du composant

--- src/app/new-card-input/new-card-input.component.ts
+++ src/app/new-card-input/new-card-input.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-card-input',
   templateUrl: './new-card-input.component.html',
-  styleUrls: ['./new-card-input.component.scss']
+  styleUrls: ['./new-card-input.component.scss'],
+  host: {'class':'col-4'}
 })
 export class NewCardInputComponent implements OnInit {
 
